420e3e255583d8d3fdd628042aeebdb95506371e
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-06-19 15:34:37 +0900

    tribial test and todo

---
+++ test/monadPlus/testMP.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE PackageImports #-}
+
+import "monads-tf" Control.Monad.State
+import "monads-tf" Control.Monad.Error
+import Control.Monad.Trans.Error (Error(..))
+
+type PackratM = StateT Int (Either String)
+
+some, some' :: PackratM Int
+some = return 8
+some' = fail "hoge"
+some'' = throwError (strMsg "hoge")
+
+other :: PackratM Int
+other = return 99
+
+sando :: PackratM Int
+sando = some'' `mplus` other
