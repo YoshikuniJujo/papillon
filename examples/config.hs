ab1ad5321b8acfccfe0b2a7377c7da12c91e41e8
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-07-21 14:49:16 +0900

    examples/config.hs の追加

---
+++ examples/config.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE QuasiQuotes, TypeFamilies #-}
+
+import Text.Papillon
+import System.Environment
+import Data.Char
+
+main :: IO ()
+main = do
+	fp : _ <- getArgs
+	cnt <- readFile fp
+	case conf $ parse cnt of
+		Right (r, _) -> print r
+		Left _ -> putStrLn "parse error"
+
+[papillon|
+
+name :: String
+	= s:(c:[isLower c] { c })+	{ s }
+;
+value :: Int
+	= ds:(d:[isDigit d] { d })+	{ read ds }
+;
+conf1 :: (String, Int)
+	= n:name ':' ' ' v:value	{ (n, v) }
+;
+conf :: [(String, Int)]
+	= cs:(c:conf1 '\n' { c })+	{ cs }
+;
+
+|]
